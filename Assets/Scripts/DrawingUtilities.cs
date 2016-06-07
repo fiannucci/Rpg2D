ebd92e8f7e719418e5d18efb3c0a14da48da06ce
Fausto Iannucci <iannucci.fausto@gmail.com>
2016-06-07 16:59:56 +0200

    - Level transitions (Fading) using drawing primitives(GL) - Battle scene - Enemy Goblin with FSM base logic - Battle Manager to spawn (smoothly) random enemies in targeted positions, and run away button - Static class to record some important data about levels and states

---
+++ Assets/Scripts/DrawingUtilities.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class DrawingUtilities
+{
+    public static void DrawQuad(Material aMaterial, Color aColor, float aAlpha)
+    {
+        aColor.a = aAlpha;
+        aMaterial.SetPass(0);
+        GL.PushMatrix();
+        GL.LoadOrtho();
+        GL.Begin(GL.QUADS);
+        GL.Color(aColor);
+        GL.Vertex3(0, 0, -1);
+        GL.Vertex3(0, 1, -1);
+        GL.Vertex3(1, 1, -1);
+        GL.Vertex3(1, 0, -1); //L'HO AGGIUNTO IO
+        GL.End();
+        GL.PopMatrix();
+    }
+}
--- Assets/Scripts/MapMovement.cs
+++ Assets/Scripts/MapMovement.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class MapMovement : MonoBehaviour {
@@ -10,11 +11,19 @@ public class MapMovement : MonoBehaviour {
     bool inputReady = true;
     bool startedTravelling = false;
     private Collider2D playerCollider;
+    
+    private int EncounterChance = 100;
+    private float EncounterDistance = 0; 
 
     void Awake()
     {
+        
         playerCollider = GetComponent<Collider2D>();
         playerCollider.enabled = false;
+        var lastPosition = GameState.GetLastScenePosition(SceneManager.GetActiveScene().name);
+
+        if (lastPosition != Vector3.zero)
+            transform.position = lastPosition;
     }
     void Start()
     {
@@ -25,39 +34,65 @@ public class MapMovement : MonoBehaviour {
     {
         inputReady = !uiVisible;
     }
+
+    void OnDestroy()
+    {
+        GameState.SetLastScenePosition(SceneManager.GetActiveScene().name, transform.position);
+    }
+
 	void Update ()
     {
         
-        if (inputActive && Input.GetMouseButtonUp(0))
+        if (inputActive && (Input.GetMouseButtonUp(0) || Input.touchCount == 1) )
         {
             StartLocation = transform.position.ToVector3_2D();
             timer = 0;
             TargetLocation = WorldExtensions.GetScreenPositionFor2D(Input.mousePosition);
             startedTravelling = true;
+
+            var EncounterProbability = Random.Range(1, 100);
+            if (EncounterProbability < EncounterChance && !GameState.PlayerReturningHome)
+            {
+                EncounterDistance = (Vector3.Distance(StartLocation, TargetLocation) / 100) * Random.Range(10, 100);
+            }
+            else
+                EncounterDistance = 0;
         }
-        else if(inputActive && Input.touchCount == 1)
+        /*else if(inputActive && Input.touchCount == 1)
         {
             StartLocation = transform.position.ToVector3_2D();
             timer = 0;
             TargetLocation = WorldExtensions.GetScreenPositionFor2D(Input.GetTouch(0).position);
             startedTravelling = true;
         }
+        */
 
         if(TargetLocation != Vector3.zero && TargetLocation != transform.position && TargetLocation!= StartLocation)
         {
             transform.position = Vector3.Lerp(StartLocation, TargetLocation, timer);
             timer += Time.deltaTime;
         }
-        if(startedTravelling && Vector3.Distance(StartLocation, transform.position.ToVector3_2D()) > 0.5f)
+
+        if(startedTravelling && Vector3.Distance(StartLocation, transform.position.ToVector3_2D()) > 0.9f)
         {
             playerCollider.enabled = true;
             startedTravelling = false;
+        }
+
+        if (EncounterDistance > 0)
+        {
+            if(Vector3.Distance(StartLocation,transform.position) > EncounterDistance)
+            {
+                TargetLocation = Vector3.zero;
+                NavigationManager.NavigateTo("Battle");
+            }
         }
         if(!inputReady && inputActive)
         {
             TargetLocation = this.transform.position;
             Debug.Log("Stopping player");
         }
+
         inputActive = inputReady;
     }
 }
